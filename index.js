c286627bf4b855f8a7ff32a93ac7afbb2d87a42e
Berta LI <you@example.com>
2021-08-23 13:34:58 +0200

    new functions and path to join hotel.js to hotel.json

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const websites = require('./websites.json');
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     for (const website of websites) {
-        const scriptPath = path.join(__dirname, 'scripts', website.scriptName);require(scriptPath)(page, website); 
+        const scriptPath = path.join(__dirname, 'scripts', website.scriptName);
+        await require(scriptPath)(page, website); 
+        console.log('Scraping done for', website.name);
     }
 })();
--- scripts/hotel.js
+++ scripts/hotel.js
@@ -1,32 +1,26 @@
-module.exports = async (page, website) => {
+const path = require('path');
+const fs = require('fs');
+
+module.exports = async(page, website) => {
+    const { selectors } = website;
+
     await page.goto(website.url);
-};
-
-const puppeteer = require('puppeteer');
-
-
-
-(async () => {
-    const browser = await puppeteer.launch({ headless: false});
-    const page = await browser.newPage();
-    await page.goto ("https://www.secure-hotel-booking.com/smart/Star-Champs-Elysees/2YXB/en/");
-
-    await page.waitForSelector("input.check-in-datepicker");
-    await page.click("input.check-in-datepicker");
-    await page.focus("input.check-in-datepicker");
-    await page.type("input.check-in-datepicker", "05 Jan 2022", { delay: 200 });
+    await page.waitForSelector(selectors.checkIn);
+    await page.click(selectors.checkIn);
+    await page.focus(selectors.checkIn);
+    await page.type(selectors.checkIn, "05 Jan 2022", { delay: 200 });
     await page.keyboard.press("Enter");
 
-    await page.waitForSelector("input.check-out-datepicker");
-    await page.click("input.check-out-datepicker", { delay: 200 });
-    await page.focus("input.check-out-datepicker");
-    await page.click("input.check-out-datepicker", { clickCount: 3 });
+    await page.waitForSelector(selectors.checkOut);
+    await page.click(selectors.checkOut, { delay: 200 });
+    await page.focus(selectors.checkOut);
+    await page.click(selectors.checkOut, { clickCount: 3 });
     await page.keyboard.press("Backspace", { delay: 300 });
-    await page.type("input.check-out-datepicker", "08 Jan 2022", { delay: 200 });
+    await page.type(selectors.checkOut, "08 Jan 2022", { delay: 200 });
     await page.keyboard.press("Enter");
 
-    await page.type("select#adults", "2", { delay: 400 });
-    await page.type("select#children", "1", { delay: 500 });
+    await page.type(selectors.adults, "2", { delay: 400 });
+    await page.type(selectors.children, "1", { delay: 500 });
     
 
     await page.click("a.this-button._skin-3._size-big._icon-right")
@@ -51,89 +45,77 @@ const puppeteer = require('puppeteer');
 
         console.log({checkOut});
 
+        await page.waitForSelector('.filters-date');
+        let date = checkoutDateTxt.format('DD-MM-YYYY')
+        /* let year = date.getFullyYear(); */
+        /* function newOrderDate(){
+            let year = date.getFullyYear();
+            let month = ("0" + (date.getFullyMonth() + 1)).slice(-2);
+            let day = ("0" + date.getDate()).slice(-2);
 
-        const [numAdults] = await page.$x('/html/body/div[3]/div/div[2]/div[1]/header/div[1]/p/span[4]/span[1]');
+            let fullDate = [year, month, day]
+            
+            return fullDate.join('-')
+            }  */ 
+
+        console.log(date); 
+
+        /* let year = d.getFullYear();
+        let monthConverter = d.getMonth(); 
+        let month = monthConverter + 1;
+        let day = d.getDate();  */
+
+
+        await page.waitForXPath(selectors.totalAdults);
+        const [numAdults] = await page.$x(selectors.totalAdults);
         const numAdultsProp = await numAdults.getProperty('textContent');
         const numAdultsTxt = await numAdultsProp.jsonValue();
         const adults = JSON.stringify(numAdultsTxt)
 
         console.log({numAdultsTxt});
 
-        const [numChildren] = await page.$x('/html/body/div[3]/div/div[2]/div[1]/header/div[1]/p/span[4]/span[4]/span[1]');
+        await page.waitForXPath(selectors.totalChildren);
+        const [numChildren] = await page.$x(selectors.totalChildren);
         const numChildrenProp = await numChildren.getProperty('textContent');
         const numChildrenTxt = await numChildrenProp.jsonValue();
         const children = JSON.stringify(numChildrenTxt)
 
         console.log({numChildrenTxt});
 
-        await page.waitForSelector('.filters-date');
-        let date = "Sat 8 Jan 2022"
-        function dateConverter(date){
-            let d = new Date(date)
-            let year = d.getFullYear();
-            let monthConverter = d.getMonth(); 
-            let month = monthConverter + 1;
-            let day = d.getDate();
-
-            let fullDate = [year, month, day]
-            
-            return fullDate.join('-')
-        }
-
-        console.log(dateConverter(date));
-
-
-        /* let totalAdults = "2"
-        let totalChildren = "1"
-        function totalGuests (totalAdults, totalChildren) {
-            return parseInt(totalAdults) + parseInt(totalChildren)
-        }
-
-        console.log(totalGuests(totalAdults)); */
-
-        /* function suma(numAdult, numChildren){
-            let numAdult = numAdult;
-            let numChildren = numChildren;
-
-            return parseInt(numAdults) + parseInt(numChildren);
-        }
-
-        document.write( suma(1,2)); */
-
+       function sum () {
+           return parseInt(numAdultsTxt) + parseInt(numChildrenTxt); 
+       }
+            console.log(sum());
+         
 
         
 
         const siteLanguage = await page.evaluate(()=>{
-        const language = document.querySelector('html').lang
+        const language = document.querySelector('html').lang;
         return language
 
         });
 
         console.log(siteLanguage);
 
-
-       /* const arrPrices = [];
-       const prices = document.querySelectorAll('.room-rates-item-price-moy');
-       prices.forEach(element = arrPrices.push(element.textContent));
-       console.log(arrPrices); */
        
-       
-        await page.waitForSelector('.room-rates-item-price-moy');
-        const rates = await page.evaluate(() => {
-        const rates = document.querySelectorAll('.room-rates-item-price-moy');
-        let arrRates = [];
-        rates.forEach(rate => {
-            arrRates.push(rate.innerText.replace('€', ' EUR'));
+        await page.waitForSelector(selectors.roomsPrices);
+        const roomsPrices = await page.evaluate(() => {
+            let arrRoomsPrices = [];
+            const roomsPrices = document.querySelectorAll('.room-rates-item-price-moy');
+            roomsPrices.forEach(roomsPrices => {
+                arrRoomsPrices.push(roomsPrices.innerText.replace('€', ' EUR'));
         });
-        return arrRates;
+            return arrRoomsPrices;
     });
 
-    console.log(rates);
+    console.log(roomsPrices);
+
 
     function lowestRateOffered() {
         const lowestRate = [];
-        rates.forEach(rate => {
-            return lowestRate.push(rate.replace(/EUR/g, ''));
+        roomsPrices.forEach(roomsPrices => {
+            return lowestRate.push(roomsPrices.replace(/EUR/g, ''));
         });
         let stringToInt = lowestRate.map(Number);
         const minRate = stringToInt.sort(function(a, b) { return a - b; });
@@ -143,37 +125,26 @@ const puppeteer = require('puppeteer');
 
     console.log(lowestPrice);
 
+    await page.waitForSelector(selectors.roomsPrices);
+    const currencyISO = await page.evaluate(() => {
+        const currencyISO = document.querySelector('.room-rates-item-price-moy');
+        const changeCode = currencyISO.innerText.replace(/\s*\u20ac\s*/ig, 'EUR');
+        return currencyISOCode = changeCode.replace(/[0-9]/g, '');
+    });
 
-    const currencyCode = await page.evaluate(() =>{
+    console.log(currencyISO)
 
-        let currencySymbols = {
-        '€':'EUR',
-        '$':'USD', 
-        '£':'GBP', 
-        'CHF': 'Swiss franc',
-        '¥':'JPY',
-        'AUD': 'Australian dolar',
-        'CAD': 'Canadian Dolar'
-        };
 
-        const allPrices = document.querySelectorAll('.room-rates-item-price-moy')
-
-        let arrPrices = []
-        allPrices.forEach(item =>{
-          arrPrices.push(item.innerText)
-        })
-
-        let currencySymbol = arrPrices[0].replace(/[\d\., ]/g, '')
-
-        let symbols = Object.keys(currencySymbols)
-        
-        let symbol = symbols.filter(item =>{
-          return item == currencySymbol
-        })
-
-        return currencySymbols[symbol]
-    
+      await page.exposeFunction('listInfoHotel', () => {
+          const bookingInfo = ({checkIn, checkOut, adults, children, siteLanguage, lowestPrice});
+          return bookingInfo;
       });
 
-      console.log(currencyCode)
-})();
+      const bookingInfo = await page.evaluate(() => {
+          return listInfoHotel();
+      });
+
+      fs.writeFileSync(
+          path.join(__dirname,`${website.scriptName}.json`), JSON.stringify(bookingInfo),'utf8'
+      );
+};
